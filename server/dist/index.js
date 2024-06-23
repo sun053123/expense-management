754c683f6c1cc064330eba076ad586d148b05ab7
Setthasat <setthasat2007@gmail.com>
2024-06-23 11:18:01 +0700

    add bd connect

--- server/dist/index.js
+++ server/dist/index.js
@@ -4,13 +4,23 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const mongoose_1 = __importDefault(require("mongoose"));
 const cors_1 = __importDefault(require("cors"));
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8888;
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
+if (process.env.DB_URL) {
+    try {
+        mongoose_1.default.connect(process.env.DB_URL);
+        console.log('DB Connected...');
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
 app.get('/', (req, res) => {
     res.send('Hello, world!');
 });
--- server/src/index.ts
+++ server/src/index.ts
@@ -6,10 +6,20 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8888;
 
 app.use(cors());
 app.use(express.json());
+
+if (process.env.DB_URL) {
+    try {
+        mongoose.connect(process.env.DB_URL);
+        console.log('DB Connected...');
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello, world!');
